ff92e84cce195671e998bd5816155a4ad7361973
__Chisato__ <113957527+chisato2233@users.noreply.github.com>
2025-12-27 19:42:34 +0800

    fix: remove C# 11 attribute

--- src/framework/Event/AutoSubscribeAttribute.cs
+++ src/framework/Event/AutoSubscribeAttribute.cs
@@ -4,7 +4,8 @@ using TH7;
 namespace GameFramework
 {
     /// <summary>
-    /// 自动订阅特性基类
+    /// 自动订阅特性
+    /// 用法: [AutoSubscribe] 或 [AutoSubscribe(typeof(PlayerDiedEvent))]
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public class AutoSubscribeAttribute : Attribute
@@ -18,21 +19,10 @@ namespace GameFramework
             TargetType = null;
         }
 
-        protected AutoSubscribeAttribute(Type targetType, AutoSubscribeTime time)
+        public AutoSubscribeAttribute(Type targetType, AutoSubscribeTime time = AutoSubscribeTime.Start)
         {
             Time = time;
             TargetType = targetType;
         }
-    }
-
-    /// <summary>
-    /// 类型化自动订阅特性，支持 IDE 搜索引用
-    /// 用法: [AutoSubscribe&lt;PlayerDiedEvent&gt;]
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public sealed class AutoSubscribeAttribute<T> : AutoSubscribeAttribute where T : struct
-    {
-        public AutoSubscribeAttribute(AutoSubscribeTime time = AutoSubscribeTime.Start)
-            : base(typeof(T), time) { }
     }
 }
--- src/framework/Event/AutoSubscribeProcessor.cs
+++ src/framework/Event/AutoSubscribeProcessor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using TH7;
 
 namespace GameFramework
 {
